3cb67453e70f947c7d97917bfe00bc57fa167144
moajili <ajilidali@gmail.com>
2024-02-02 20:36:12 +0100

    ss

--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@
 /*   By: moajili <moajili@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 12:29:30 by hclaude           #+#    #+#             */
-/*   Updated: 2024/02/02 20:31:50 by moajili          ###   ########.fr       */
+/*   Updated: 2024/02/02 20:36:05 by moajili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
-
 
 
 int main(int argc, char **argv)
