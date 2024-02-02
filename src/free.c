e6409e84c654064cec89a98f3cdb6ede851b4590
Hippolyte Claude <hclaude@k2r2p2.42mulhouse.fr>
2024-02-02 16:16:05 +0100

    C'est plus clair dans le fichier free.c

--- src/free.c
+++ src/free.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moajili <moajili@student.42mulhouse.fr>    +#+  +:+       +#+        */
+/*   By: hclaude <hclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 15:53:51 by moajili           #+#    #+#             */
-/*   Updated: 2024/02/02 15:56:25 by moajili          ###   ########.fr       */
+/*   Updated: 2024/02/02 16:15:52 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
+
+// free la chaine de int (pos)
 
 void	free_int(t_fdf *map)
 {
@@ -22,6 +24,8 @@ void	free_int(t_fdf *map)
 	free(map->pos);
 }
 
+// free la chaine faite par split
+
 void ft_freesplit(char **split_result)
 {
     int i;
@@ -32,6 +36,8 @@ void ft_freesplit(char **split_result)
     free(split_result);
     split_result = NULL;
 }
+
+// free le tableau content
 
 void	free_char(t_fdf *map)
 {
