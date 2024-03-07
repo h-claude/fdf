2e8e08cc1a57e02c9bc22d4225bbf153eceefc55
h-claude <hippolyteclaude@icloud.com>
2024-03-07 16:34:20 +0100

    Debug couleur

--- src/get_color.c
+++ src/get_color.c
@@ -6,7 +6,7 @@
 /*   By: hclaude <hclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 14:10:34 by hclaude           #+#    #+#             */
-/*   Updated: 2024/02/26 16:37:28 by hclaude          ###   ########.fr       */
+/*   Updated: 2024/03/07 16:31:24 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,12 +45,14 @@ int32_t	get_the_color(char *hexStr)
 	put_lowercase(hexStr + 2);
 	result = 0;
 	hexStr += 2;
-	while (*hexStr)
+	while (*hexStr && *hexStr != '\n')
 	{
 		val = hexa_to_int(*hexStr++);
 		if (val == -1)
 			return (-1);
 		result = (result << 4) | val;
 	}
+	if (strlen(hexStr) < 8)
+		result = (result << 8) | 0xFF;
 	return (result);
 }
